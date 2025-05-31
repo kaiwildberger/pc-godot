7dbfe8c807f75b006cfe58140d90578078eac9bc
Kai Wildberger <kiawildberger@gmail.com>
2025-05-31 16:04:23 -0700

    filesystem organization

--- scripts/tower.gd
+++ scripts/tower.gd
@@ -1,5 +1,9 @@
 extends Node2D
 
+@export var eating_cooldown = 3 # cooldown in seconds
+var is_timedout = false
+var timer;
+
 func _ready() -> void:
 	#print('tower spanwed')
 	pass
@@ -8,5 +12,11 @@ func _process(delta: float) -> void:
 	pass
 
 func _on_range_area_entered(area: Area2D) -> void:
-	area.get_parent().emit_signal("sushi_eaten")
-	# create timer for Chewing cooldown
+	if is_timedout == true:
+		#print("too full... let one pass :P")
+		pass
+	else:
+		area.get_parent().emit_signal("sushi_eaten")
+		timer = get_tree().create_timer(eating_cooldown) # could be Global for upgrades but...
+		is_timedout = true
+		timer.timeout.connect(func(): is_timedout = false)
